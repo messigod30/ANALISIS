25478be0d62f0e634b61150902038765c288cb2d
samuel gil <samuelgilh062@gmail.com>
2022-03-08 13:16:22 -0500

    solucion taller 2

--- index.js
+++ index.js
@@ -44,3 +44,69 @@ console.log(valor);
 // concatenado
 console.log( "messi "+ valor)
 prompt("ingrese un valor: ");
+
+// operadores
+// suma
+var suma; 
+suma= 5+4 
+console.log (suma);
+var a = 12;
+suma=a+8;
+console.log (suma);
+var b=20;
+suma= a+b;
+console.log (suma);
+// resta
+var resultado;
+resultado=a-b;
+console.log(resultado)
+resultado= b-a ;
+console.log (resultado);
+// multiplicacion
+var producto;
+producto= a*b;
+console.log (producto);
+// division
+var division;
+division= a/b;
+console.log (division);
+division=b/a;
+console.log (division);
+// orden de operaciones
+var resultado;
+resultado = a*b;
+console.log(resultado);
+resultado=a*b-10;
+console.log(resultado);
+resultado=a*(b-10);
+console.log (resultado)
+// exponente/potencia
+var exp= 3**2;
+console.log(exp);
+exp=3**3;
+console.log(exp)
+// cadenas o string
+var cadenita1="3";
+var cadenita2="5";
+var sumacadenas=cadenita1 + cadenita2;
+console.log (sumacadenas);
+var nombre = "Samuel";
+var apellido = "Gil";
+var nombreCompleto = nombre+" "+ apellido;
+console.log (nombreCompleto);
+var longitud = nombre.length;
+console.log(longitud);
+console.log(nombreCompleto.length);
+// encontrar caracteres dada la posicion del vector
+var primeraLetra;
+primeraLetra= nombre[0];
+console.log(primeraLetra);
+console.log(nombre[1]);
+// encontrar la ultima posicion de una cadena o vector
+console.log(nombre[nombre.length-1]);
+// se debe buscar la longitud de la cadena y restar para encontrar la ultima letra 
+// las posiciones de una cadena se pueden consultar pero no se pueden reasignar un valor
+nombre[0]="P";
+console.log(nombre);
+nombre="Lionel";
+console.log(nombre);
