f2fe10faa7b6471c38194e8e7b24e342ca9ccd57
samuel gil <samuelgilh062@gmail.com>
2022-03-09 08:09:20 -0500

    solucion taller 2

---
+++ taller2.js
@@ -0,0 +1,48 @@
+//taller 2 cuadrado
+// punto1
+var areaCua;
+ areaCua=prompt("ingrese la medida de un lado del cuadrado");
+areaCua= areaCua**2;
+console.log ("el area de su cuadrado es", areaCua);
+var volumenCu;
+ volumenCu=prompt("ingrese la medida de un lado del cubo");
+ volumenCu=volumenCu**3;
+console.log ("el volumen de su cubo es", volumenCu);
+// punto2
+var rectanguloB;
+rectanguloB=prompt("ingrese la base del rectangulo")
+var rectanguloH
+rectanguloH=prompt("ingrese la altura del rectangulo")
+var areaRentangulo= rectanguloB*rectanguloH
+console.log("el area del rectangulo es ", areaRentangulo);
+var ladoR1;
+ ladoR1=prompt("ingrese la medida de un lateral ");
+var ladoR2;
+ladoR2=prompt("ingrese la medida del superior del rectangulo");
+var perimetro;
+perimetro= (ladoR1+ladoR2+ladoR1+ladoR2);//ladoR1+ladoR1+ladoR2+ladoR2;
+console.log ("el perimetro del rectangulo es ", perimetro);
+// punto3
+var radioCirculo;
+radioCirculo=prompt("ingrese el radio del circulo");
+radioCirculo= radioCirculo**2*3.1416;
+console.log("el area de es cuadrado es ", radioCirculo);
+var volumenEsfera;
+volumenEsfera=prompt("ingrese el valor del radio de la esfera ");
+volumenEsfera= (4/3)*3.1416*(volumenEsfera**3);
+console.log ("el volumen de su esfera es ", volumenEsfera); 
+// punto4
+var base1;
+var base2;
+var h;
+base1=prompt ("ingrese la primer base");
+base2=prompt ("ingrese la segunda base");
+h=prompt ("ingrese la altura");
+// revisar formula
+var areaTrapezoide= 1/2*(base1+base2)*h
+console.log ("el area de su trapezoide es", areaTrapezoide);
+// punto5
+var largoPiscina
+var anchoPiscina
+var profundidadPiscina
+largoPiscina=prompt
