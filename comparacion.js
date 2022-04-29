929eed93636da3e0b28a232ea21f89ea5e34df01
Samuel G <samuelgilh062@gmail.com>
2022-04-29 11:34:26 -0500

    nuevo

---
+++ comparacion.js
@@ -0,0 +1,59 @@
+console.log(2 < 12); //true
+console.log(2 < "12"); //false	
+console.log(2 < "John");//false	
+console.log(2 > "John");//false
+console.log(2 == "John");//false
+console.log("John" == "John");//true
+console.log("john" == "John");//false		
+console.log("2" < "12");	//true
+console.log("2" > "12");	//false
+console.log("2" == "12");//false
+
+console.log("ejercicios")
+x=12;   
+y=7;
+
+x<15 && y!=7 //f
+y>8 && x<20 //f
+x==12 && x<18 //t
+"lunes" == "Lunes" || y!=9 //t
+!(x==10) && y>=6 //t
+x>6 && y==12 && 2>3 //f
+(x*2)>22 && y<(2*4) //t
+(x>10 || x<10) && !(y!=7 && y<21) //t
+
+var tocaEduFisica= "si";
+var uniforme = "diario";
+ 
+if(tocaEduFisica == "no"){
+    uniforme = "edu. física";
+}
+ 
+console.log("Mañana debe vestir el uniforme de " + uniforme);
+
+var hayGaseosa = "no";
+var bebida;
+ 
+if(hayGaseosa == "si"){
+    bebida = "gaseosa";
+}else{
+    bebida = "refresco de fruta"
+}
+ 
+console.log("la bebida que acompaña su pedido es " + bebida);
+
+
+var diaSiguiente= "lunes";
+var cuaderno;
+ 
+if(diaSiguiente == "lunes" || diaSiguiente == "martes"){
+    cuaderno="Análisis";
+}else if(diaSiguiente == "miercoles"){
+    cuaderno="Tecnología";
+}else if(diaSiguiente == "jueves"){
+    cuaderno="Desarrollo"
+}else{
+    cuaderno="Ninguno";
+}
+ 
+console.log("Para la clase de mañana con el profesor Andres debe guardar el cuaderno de" +cuaderno);
---
+++ taller comparaciones.js
@@ -0,0 +1,115 @@
+// primero
+var saludar="si";
+ 
+if(saludar=="si"){
+    console.log("Hola, soy un condicional simple");
+}
+//segundo punto
+var x=20;
+ 
+if(x<=20){
+    console.log("La condición es verdadera");
+}
+// punto 3
+var edad=18;
+ 
+if(edad>=18){	
+    console.log("Bienvenido, puede ingresar");
+}else{
+    console.log("Lo siento, no tiene edad suficiente para entrar aquí");
+}
+
+// punto 4 
+
+var numero1 = 5;
+var numero2 = 8;
+ 
+if(numero1<numero2) {
+  console.log("numero1 no es mayor que numero2");
+}
+if(numero2>0) {
+  console.log("numero2 es positivo");
+}
+if(numero1!==0) {
+  console.log("numero1 es negativo o distinto de cero");
+}
+if(numero1<numero2) {
+  console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+// punto 5
+var numerito = 100;
+ 
+if(numerito<50) {
+  console.log("numerito sí es estrictamente menor que 50");
+}else if(numerito>=50 && numerito<=80) {
+  console.log("numerito sí está entre 50 y 80");
+}else if(numerito>80 && numerito<100) {
+  console.log("numerito es mayor que 80 pero no mayor ni igual a 100");
+}else if(numerito==100) {
+  console.log("numerito es igual a 100");
+}else{
+    console.log("numerito es estrictamente mayor a 100");
+}
+// punto 6
+
+numerito=90
+
+// punto 7
+numerito=70
+
+// punto 8 
+numerito=120
+
+// punto 9
+var dia = "jueves ";
+
+if(dia == "jueves") {
+     console.log("vaya al poli");
+}else{
+    console.log("no puede ir al poli");
+  }
+// punto 10
+var num1 =12 ;  
+var num2 =7;
+var resultado;
+    
+if(num2 == 0 ){
+ console.log("no se puede dividir");
+}else{
+    console.log(num1/num2);
+}
+
+// punto 11
+
+var quedia;
+
+if(quedia== "lunes"){
+    console.log("arroz con pollo");
+}else if (quedia== "martes"){
+    console.log("chuleta de cerdo/pollo");
+}else if (quedia== "miercoles"){
+    console.log("frijoles");
+}else if (quedia== "jueves"){
+    console.log("sancocho de pescado");
+}else if (quedia== "viernes"){
+    console.log("bandeja paisa");
+}else if (quedia== "sabado"){
+    console.log("Ajiaco");
+}else if (quedia== "domingo"){
+    console.log("sancocho de gallina");
+}
+
+//punto 12
+  var usuario=4;
+if(usuario== "a" || usuario == "e" || usuario== "i" || usuario== "o" || usuario== "u" ){
+  console.log("es una vocal");
+}else if ( usuario==0 || usuario== 1 || usuario == 2 || usuario == 3 || usuario==4 || usuario== 5 || usuario== 6 || usuario== 7 
+|| usuario== 8 || usuario== 9){
+  console.log("es un numero");
+}else{
+  usuario=="consonante";
+console.log("es consonante");
+}
+
+
+ 
