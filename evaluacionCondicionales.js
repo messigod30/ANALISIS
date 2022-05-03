1e45bf763397d967718f377c8c6c7f3dea509d72
Samuel G <samuelgilh062@gmail.com>
2022-05-03 13:40:03 -0500

    examen

---
+++ evaluacionCondicionales.js
@@ -0,0 +1,63 @@
+// samuel gil y juan leon
+// punto 1
+var variable = "Valor";
+ 
+if (variable=="Valor"){
+   console.log("La condición es verdadera");
+}
+// punto 2
+var valor= 90 ;
+ 
+if (valor < 100 && valor >= 90){
+   console.log("El valor es correcto");
+}
+// punto 3
+var numero = 10;
+
+if (numero<0){
+   console.log("El número no es positivo");
+}else if(numero==="1"){
+    console.log('El número es igual a "1" en tipo y valor');
+}else if(numero>=1 && numero<10){
+    console.log("El número es mayor igual que 1 y menor igual a 10");
+}else if(numero<100){
+    console.log("El número es menor que 100");
+}
+
+//Respuesta 3.A
+   numero= -8
+//Respuesta 3.B
+    numero="1"
+//Respuesta 3.C
+    numero=1
+//Respuesta 3.D
+    numero=10
+ 
+// punto 4
+var numerote1 =7;
+var numerote2 =8;
+
+if(numerote1<numerote2){
+    console.log("numerote1 es menor que numerote2");
+}else if (numerote2<numerote1){
+    console.log("numerote2 es menor que numerote1");
+}else if (numerote1==numerote2){
+    console.log("numerote1 y numerote2 son iguales");
+}
+// punto 5
+
+var palabra = "house";
+
+if (palabra=="red" ){
+    console.log("la traduccion de su palabra es rojo ");
+}else if(palabra== "car"){
+    console.log("la traduccion de su palabra es carro");
+}else if(palabra== "house"){
+    console.log("la traduccion de su palabra es casa");
+}else if(palabra== "dog"){
+    console.log("la traduccion de su palabra es perro");
+}
+
+
+
+    
